547f9bb12849cfeb3bf5db15d64a06a6bf60760d
NgHoangHao <haonguyenhoang8@gmail.com>
2025-06-12 10:03:16 +0700

    update

--- BusServicePro/src/main/java/com/example/controller/viewFeedback.java
+++ BusServicePro/src/main/java/com/example/controller/viewFeedback.java
@@ -3,10 +3,7 @@ package com.example.controller;
 import com.example.DAO.FeedbackDAO;
 import com.example.models.Feedback;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -17,7 +14,7 @@ public class viewFeedback {
     @FXML
     private TextField user;
     @FXML
-    private TextField feedback;
+    private TextArea feedback;
     public void initialize() {
           user.setText(adminFeedback.getSelectedName());
           feedback.setText(adminFeedback.getSelectedMessage());
